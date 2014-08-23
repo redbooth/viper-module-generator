d752e5b4c49939fb58a1e3ffc80c1571a0c5bbe5
Pedro Piñera <pepibumur@gmail.com>
2014-08-23 23:18:17 +0200

    Generator is working!"

--- lib/vipergen/filemanager.rb
+++ lib/vipergen/filemanager.rb
@@ -26,24 +26,29 @@ module Vipergen
 		# Returns an array with files in a given path
 		# @return Array with the files in a given path
 		def self.files_in_path(path)
-			return Dir["#{path}**/*"].select {|f| File.file?(f)}
+			return Dir[File.join("#{path}","/**/*")].select {|f| File.file?(f)}
 		end
 
 		# Returns the destination viper path 
 		# @return Destination root path
 		def self.destination_viper_path(path, name)
-			return File.join(path,name)
+			expand_path = File.expand_path(path)
+			return File.join(expand_path,name)
 		end
 
 		# Copy a system item to another place
 		def self.copy(from, to)
-			FileUtils.mkdir_p (to)
-			FileUtils.copy_entry(from, to)	
+			to_expand_path = File.expand_path(to)
+			from_expand_path = File.expand_path(from)
+			FileUtils.mkdir_p (to_expand_path)
+			FileUtils.copy_entry(from_expand_path, to_expand_path)	
 		end
 
 		# Move a system item to another place
 		def self.move(from, to)
-			FileUtils.move(from, to)
+			to_expand_path = File.expand_path(to)
+			from_expand_path = File.expand_path(from)
+			FileUtils.move(from_expand_path, to_expand_path)
 		end
 
 	end
